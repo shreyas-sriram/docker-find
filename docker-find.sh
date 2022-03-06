1e10d993e14aace5ef51a67d62293bd89f9579d8
Shreyas Sriram <shreyassriram99@gmail.com>
2022-03-06 15:10:33 -0500

    Refine search

--- docker-find.sh
+++ docker-find.sh
@@ -6,17 +6,46 @@ SCRIPT_NAME="docker-find"
 LAYERS="layers"
 LAYER_TAR_FILENAME="${LAYERS}.tar"
 
+# clean up before starting
+function clean_up(){
+    printf "[+] Cleaning existing files\n\n"
+
+    rm -rf ${LAYERS}
+}
+
+# find image locally
+# if not available locally, pull it
+function find_image(){
+    printf "[+] Searching '${image}' locally\n\n"
+    
+    if [[ "$(docker images -q ${image} 2> /dev/null)" == "" ]]; then
+        printf "[-] Docker image: '${image}' not found locally\n"
+        
+        printf "[+] Pulling image: '${image}'\n"
+        docker pull -q ${image}
+    fi
+
+    printf "[+] Found '${image}' locally\n\n"
+}
+
+# search for `search_term` in `docker history`
 function search_history(){
-    echo "Searching docker history"
+    printf "[+] Searching docker history\n\n"
+
     docker history ${image} --no-trunc | grep --color -i ${search_term}
 }
 
+# search for search_term in `docker inspect`
 function search_inspect(){
-    echo "Searching docker inspect"
+    printf "[+] Searching docker inspect\n\n"
+    
     docker inspect ${image} | grep --color -i ${search_term}
 }
 
+# save image layers using `docker save` and untar each layers
 function save_and_untar_layers(){
+    printf "[+] Saving and extracting layers\n\n"
+
     mkdir -p ${LAYERS}
 
     docker save ${image} -o ${LAYER_TAR_FILENAME}
@@ -24,28 +53,49 @@ function save_and_untar_layers(){
     
     layers=(${LAYERS}/*/)
     for layer in "${layers[@]}"; do
-        mkdir ${layer}/layer
+        mkdir -p ${layer}layer # /layers/<layer-name>/layer
         tar xf ${layer}/layer.tar -C ${layer}/layer
     done
 }
 
+# recursive grep search
+function grep_search(){
+    printf "[+] Running 'grep' on layer files\n\n"
+
+    grep -HnrIi --color "${search_term}" ${LAYERS}/
+}
+
+# recursive strings search
+function strings_search(){
+    printf "[+] Running 'strings' on layer files\n\n"
+
+    find ${LAYERS}/ -type f \( ! -iname "*.tar" \) | xargs strings -f | grep --color -i "${search_term}"
+}
+
+# search for `search_term` in extracted layers
 function search_layers(){
     save_and_untar_layers
 
-    # grep search
-    # strings search
+    printf "[+] Searching layers\n\n"
+
+    grep_search
+    strings_search
 }
 
 function main(){
-    echo "Searching ${image} for ${search_term}"
+    clean_up
+
+    find_image
+
+    printf "[+] Searching '${image}' for '${search_term}'\n\n"
     
-    # search_history
-    # search_inspect
+    search_history
+    search_inspect
     search_layers
 }
 
 if [[ "$#" -ne 2 ]]; then
-    echo "Script requires 2 arguments: ./${SCRIPT_NAME} [image] [search-term]"
+    printf "[-] Script requires 2 arguments: ./${SCRIPT_NAME} [image] [search-term]\n\n"
     exit
 fi
 
